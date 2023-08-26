ec88d24ac4c195b6fb4a2782d0d35e183cc046f3
hao <41532093+ywluohao@users.noreply.github.com>
2023-08-26 03:17:26 -0400

    Create progress_bar_visibility.vue

---
+++ progress_bar_visibility.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <Button @click="startLoading">Start Loading</Button>
+    
+    <ProgressBar v-if="showProgressBar" :progress="overallProgress" />
+
+    <div>{{ loadingMessage }}</div>
+  </div>
+</template>
+
+<script>
+import Button from './Button.vue'; // Import your Button component
+import ProgressBar from './ProgressBar.vue'; // Import your ProgressBar component
+
+export default {
+  components: {
+    Button,
+    ProgressBar
+  },
+  data() {
+    return {
+      showProgressBar: false,
+      loadingMessage: '',
+      tables: ['Table1', 'Table2'], // Names of the tables to load
+      overallProgress: 0
+    };
+  },
+  methods: {
+    async startLoading() {
+      this.showProgressBar = true;
+      this.loadingMessage = 'Loading...';
+      
+      await this.loadDataFromTables();
+
+      this.loadingMessage = 'Loading complete!';
+      this.showProgressBar = false;
+    },
+    // ... other methods as in the previous example
+  }
+};
+</script>
