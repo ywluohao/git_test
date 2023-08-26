54fec18fd2864a1ec48b8bb2cc5ec3b4f81602aa
hao <41532093+ywluohao@users.noreply.github.com>
2023-08-26 01:22:13 -0400

    Create page.vue

---
+++ vue/page.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-tabs v-model="tab">
+      <v-tab>Tab 1</v-tab>
+      <v-tab>Tab 2</v-tab>
+    </v-tabs>
+    <v-tab-item v-for="(component, index) in tabComponents" :key="index">
+      <component :is="component"></component>
+    </v-tab-item>
+  </div>
+</template>
+
+<script>
+import Tab1 from './Tab1.vue';
+import Tab2 from './Tab2.vue';
+
+export default {
+  components: {
+    Tab1,
+    Tab2
+  },
+  data() {
+    return {
+      tab: 0,
+      tabComponents: [Tab1, Tab2]
+    };
+  }
+};
+</script>
