305712e7b6163f8bd51e3ba915db7ad8a16f7eef
hao <41532093+ywluohao@users.noreply.github.com>
2023-08-26 01:23:41 -0400

    Create tab1.vue

---
+++ vue/tab1.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <pre>{{ jsonData }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jsonData: null
+    };
+  },
+  mounted() {
+    this.loadJsonData();
+  },
+  methods: {
+    async loadJsonData() {
+      try {
+        const response = await fetch('path/to/your/json/file.json');
+        this.jsonData = await response.json();
+      } catch (error) {
+        console.error('Error loading JSON data:', error);
+      }
+    }
+  }
+};
+</script>
