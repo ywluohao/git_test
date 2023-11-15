bc75155ac77c14b194b4862475457a719a5c1488
hao <41532093+ywluohao@users.noreply.github.com>
2023-11-14 22:42:44 -0500

    Update 1.vue

---
+++ 1.vue
@@ -0,0 +1,34 @@
+// Install xlsx library
+// npm install xlsx
+
+// Import the necessary modules
+import * as XLSX from 'xlsx';
+
+export default {
+  methods: {
+    // Function to trigger Excel download
+    downloadExcel() {
+      // Get JSON data from Vuex store
+      const jsonDataSheet1 = this.$store.state.dataSheet1; // Replace 'dataSheet1' with the actual state name
+      const jsonDataSheet2 = this.$store.state.dataSheet2; // Replace 'dataSheet2' with the actual state name
+
+      // Convert JSON data to Excel worksheets
+      const wsSheet1 = XLSX.utils.json_to_sheet(jsonDataSheet1);
+      const wsSheet2 = XLSX.utils.json_to_sheet(jsonDataSheet2);
+
+      // Create Excel workbook and add sheets
+      const wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, wsSheet1, 'Sheet 1');
+      XLSX.utils.book_append_sheet(wb, wsSheet2, 'Sheet 2');
+
+      // Create a blob from the workbook
+      const blob = XLSX.write(wb, { bookType: 'xlsx', type: 'blob', mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+
+      // Create a link element and trigger the download
+      const link = document.createElement('a');
+      link.href = window.URL.createObjectURL(blob);
+      link.download = 'combined_data.xlsx';
+      link.click();
+    },
+  },
+};
