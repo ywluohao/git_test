03667b5395af53e020b797042b6fcefbf24fe78a
hao <41532093+ywluohao@users.noreply.github.com>
2025-05-05 00:58:28 -0400

    Create 20250501.vue

---
+++ vue/20250501.vue
@@ -0,0 +1,3 @@
+let dateObj = new Date(this.date.slice(0,4), this.date.slice(4,6)-1, this.date.slice(6,8));
+dateObj.setDate(dateObj.getDate() - 1);
+let result = dateObj.toISOString().slice(0,10).replace(/-/g, '');
