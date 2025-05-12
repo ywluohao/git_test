411f07416eca51d7f39d68fa95105557643ad634
hao <41532093+ywluohao@users.noreply.github.com>
2025-05-12 18:48:17 -0400

    Update 202505.sh

--- 202505.sh
+++ 202505.sh
@@ -2,3 +2,10 @@ pip download pandas --python-version 3.13 --abi cp313
 
 
 pip install --no-index --find-links=. pandas
+
+
+
+pip download pandas --python-version 3.13 --platform manylinux2014_x86_64 --implementation cp --abi cp313 --only-binary=:all:
+
+
+pip install --no-index --find-links=. pandas
