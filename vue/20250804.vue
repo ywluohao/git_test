0e352a19bb73e3363d99808330c8939c1bef7418
hao <41532093+ywluohao@users.noreply.github.com>
2025-08-03 21:57:28 -0400

    Create 20250804.vue

---
+++ vue/20250804.vue
@@ -0,0 +1,10 @@
+<div class="d-flex flex-wrap">
+  <b-list-group-item
+    v-for="field in column.iaap"
+    :key="field"
+    class="w-50 d-flex justify-content-between"
+  >
+    <strong>{{ field }}</strong>
+    <span>{{ df.iaap[0][field] }}</span>
+  </b-list-group-item>
+</div>
